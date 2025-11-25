285897c8e4157442583923462a7ac68c9a571168
unknown <Brandundundunn>
2025-11-25 15:00:44 +0700

    Upload assignment

---
+++ lang/en/courses.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'title' => 'Our Courses',
+    'subtitle' => 'Learn practical skills for the real world.',
+    'price_label' => 'Price',
+    'duration' => ':weeks weeks',
+    'seats' => '{0} Fully booked|{1} :count seat left|[2,*] :count seats left',
+    'welcome' => 'Welcome, :name!',
+];
---
+++ resources/views/product.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ __('product.title') }}</title>
+</head>
+<body>
+    <h1>{{ __('messages.title') }}</h1>
+    <h1>{{ __('messages.price') }}</h1>
+    <p>{{ __('messages.description') }}</p>
+
+    Choose Language:
+    <a href="/id/product">🇮Bahasa Indonesia</a> |
+    <a href="/en/product">English</a>
+</body>
+</html>
